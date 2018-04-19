542d621c62807b7f1e08086da1aa1a7ccc4f66e9
Sam <s.kataev@slemma.com>
2018-04-19 16:58:45 +0500

    usage of properties file

--- src/main/java/com/tests/ui/slemmauitests/LoginAndRegister.java
+++ src/main/java/com/tests/ui/slemmauitests/LoginAndRegister.java
@@ -1,6 +1,7 @@
 package com.tests.ui.slemmauitests;
 
 import com.objects.slemmaobjects.pageobjects.AuthPage;
+import com.service.TestProperties;
 import com.tests.ui.WebTest;
 import org.testng.annotations.Test;
 
@@ -9,7 +10,7 @@ public class LoginAndRegister extends WebTest {
     @Test
     public void login(){
         AuthPage page = new AuthPage(baseUrl, webDriver);
-        page.logIn("","");
+        page.logIn(TestProperties.getProp("email"), TestProperties.getProp("password"));
     }
 
 }
