bfef36d4ae138f730dd79d83be48c95743bd2588
Sam <s.kataev@slemma.com>
2018-05-24 11:38:30 +0500

    get ignored props from test suite parameter(validate response body)

--- src/main/java/com/tests/http/HttpTestsRuntime.java
+++ src/main/java/com/tests/http/HttpTestsRuntime.java
@@ -8,6 +8,8 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class HttpTestsRuntime {
@@ -28,16 +30,17 @@ public class HttpTestsRuntime {
             setRequest(request);
         }
 
+        @Parameters({"ignoredProps"})
         @Test
-        public void runTest(){
+        public void runTest(@Optional("") String ignoredProps){
             Assert.assertTrue(request.send(client));
             Assert.assertTrue(request.validateStatusCode());
-            ArrayList<String> ignoredProps = new ArrayList<>();
-            ignoredProps.add("CreatedAt");
-            ignoredProps.add("SIG");
-            ignoredProps.add("Token");
-            ignoredProps.add("Key");
-            Assert.assertTrue(request.validateResponseBody(ignoredProps));
+            Assert.assertTrue(request.validateResponseBody(getIgnoredProps(ignoredProps)));
+        }
+
+
+        ArrayList<String> getIgnoredProps(String ignoredProps) {
+            return new ArrayList<>(Arrays.asList(ignoredProps.split(";")));
         }
 
         void setRequest(HttpRequestWrapper request) {
